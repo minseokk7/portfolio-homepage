7b55d323c7362a65ffba6f02bfa2291c9f126775
minseok <71415819+minseokk7@users.noreply.github.com>
2026-05-11 12:33:03 +0900

    Configure custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/portfolio-homepage/",
+  base: "/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
